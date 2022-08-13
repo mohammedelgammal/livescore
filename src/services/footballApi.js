2762f67a1707389be80ef45385205c06b260f255
mohammed_elgammal <you@example.com>
2022-08-13 19:50:45 +0200

    mod_baseUrl

--- src/services/footballApi.js
+++ src/services/footballApi.js
@@ -2,11 +2,11 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 const dynamicBaseQuery = async (args, webApi, extraOptions) => {
   const sportUrl = webApi.getState().football.sportUrl;
-  const baseUrl = `https://${sportUrl}.api-sports.iosss`;
+  const baseUrl = `https://${sportUrl}.api-sports.io`;
   const rawBaseQuery = fetchBaseQuery({
     baseUrl,
     prepareHeaders: (headers) => {
-      headers.set("x-rapidapi-host", `${sportUrl}.api-sports.iosss`);
+      headers.set("x-rapidapi-host", `${sportUrl}.api-sports.io`);
       headers.set("x-rapidapi-key", process.env.REACT_APP_FOOTBALL_API_KEY);
       return headers;
     },
