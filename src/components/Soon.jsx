d82fb79ab4b7aa7058128a6cdefe1fb85db486dc
mohammed_elgammal <you@example.com>
2022-08-13 17:24:56 +0200

    header_container_modified

--- src/components/Soon.jsx
+++ src/components/Soon.jsx
@@ -22,19 +22,26 @@ const Soon = ({ sport }) => {
       <Center w={"100%"} h={"100%"}>
         <Stack direction={"column"} spacing={10} mt={20}>
           <Image objectFit="cover" src={workingImg} alt="Working" />
-          <Heading as="h2" size="2xl">
-            {sport} is Coming Soon...
-          </Heading>
-          <Text fontSize={15} color={"gray.400"}>
-            Stay tuned for more sports coming. I am working on a different cool
-            feature. It's going to more sophisticated and simple.
-          </Text>
+          <Stack spacing={4}>
+            <Heading as="h2" size="2xl">
+              {sport} is Coming Soon...
+            </Heading>
+            <Text fontSize={15} color={"gray.400"}>
+              Stay tuned for more sports coming. I am working on a different
+              cool feature. It's going to more sophisticated and simple.
+            </Text>
+          </Stack>
           <Stack direction={"row"}>
             <Button borderRadius={"100px"} onClick={goBack}>
               <IoReturnUpBackOutline />
             </Button>
-            <Button w={"100px"} borderRadius={"100px"} onClick={goHome}>
-              <FaHome />
+            <Button
+              bg={"blue.700"}
+              w={"100px"}
+              borderRadius={"100px"}
+              onClick={goHome}
+            >
+              <FaHome color="white" />
             </Button>
           </Stack>
         </Stack>
--- src/containers/Header.jsx
+++ src/containers/Header.jsx
@@ -1,28 +1,35 @@
-import { Container, Grid, GridItem } from "@chakra-ui/react";
+import { Container, Grid, GridItem, Center } from "@chakra-ui/react";
 import Navbar from "./../components/Navbar";
 import Searchbar from "./../components/Searchbar";
 import Logo from "./../components/Logo";
 
 const Header = () => {
   return (
-    <Container className="header-container" maxW={"1280px"} bg={"blue.700"}>
-      <Grid
-        templateAreas={`"logo search nav"`}
-        gridTemplateRows={"1fr"}
-        gridTemplateColumns={"180px 3fr 1fr"}
-        gap="5"
+    <Center w={"100vw"} height={"60px"} bg={"blue.700"}>
+      <Container
+        className="header-container"
+        p={0}
+        maxW={"1280px"}
+        bg={"blue.700"}
       >
-        <GridItem area={"logo"}>
-          <Logo />
-        </GridItem>
-        <GridItem area={"search"}>
-          <Searchbar />
-        </GridItem>
-        <GridItem area={"nav"}>
-          <Navbar />
-        </GridItem>
-      </Grid>
-    </Container>
+        <Grid
+          templateAreas={`"logo search nav"`}
+          gridTemplateRows={"1fr"}
+          gridTemplateColumns={"180px 3fr 1fr"}
+          gap="5"
+        >
+          <GridItem area={"logo"}>
+            <Logo />
+          </GridItem>
+          <GridItem area={"search"}>
+            <Searchbar />
+          </GridItem>
+          <GridItem area={"nav"}>
+            <Navbar />
+          </GridItem>
+        </Grid>
+      </Container>
+    </Center>
   );
 };
 
