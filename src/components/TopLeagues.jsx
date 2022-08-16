3772eb6c66f49e2a508b964a8caa056ea711d5cb
mohammed_elgammal <you@example.com>
2022-08-16 22:07:27 +0200

    modified_importing_imgs_confilcts_with_jest

--- src/components/TopLeagues.jsx
+++
@@ -1,8 +0,0 @@
-import List from "../components/List";
-import { topLeagues } from "../data/local";
-
-const Rankings = () => {
-  return <List title="TOP LEAGUES" data={topLeagues} apiType="rapid" />;
-};
-
-export default Rankings;
